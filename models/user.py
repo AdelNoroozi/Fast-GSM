3617a5a26502ea83db56fe80dbb9c395efc858ff
AdelNoroozi <adel13no@gmail.com>
2023-09-04 14:53:41 +0330

    feat(back): user model added

---
+++ models/user.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+
+from db import metadata
+from models.enums import RoleEnum
+
+user = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String, unique=True, nullable=False),
+    sqlalchemy.Column("public_name", sqlalchemy.String(20)),
+    sqlalchemy.Column("role", sqlalchemy.Enum(RoleEnum), server_default=RoleEnum.observer.name, nullable=False),
+    sqlalchemy.Column("phone_number", sqlalchemy.String(20)),
+    sqlalchemy.Column("password", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("joined_at", sqlalchemy.DateTime, server_default=sqlalchemy.func.utcnow(), nullable=False),
+    sqlalchemy.Column(
+        "modified_at", sqlalchemy.DateTime,
+        server_default=sqlalchemy.func.utcnow(),
+        on_update=sqlalchemy.func.utcnow(),
+        nullable=False
+    )
+)
