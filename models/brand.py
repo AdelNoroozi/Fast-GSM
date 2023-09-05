4b2de99a899cf482e4c3dd773fb3d916f752e538
AdelNoroozi <adel13no@gmail.com>
2023-09-05 14:28:46 +0330

    fix(back): modification on brand table

--- models/brand.py
+++ models/brand.py
@@ -5,6 +5,6 @@ brand = sqlalchemy.Table(
     "brands",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(20), nullable=False),
+    sqlalchemy.Column("name", sqlalchemy.String(20), nullable=False, unique=True),
     sqlalchemy.Column("logo_url", sqlalchemy.String(100)),
 )
