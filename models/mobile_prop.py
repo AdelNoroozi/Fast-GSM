5d9d7e9e79a360e7c85b406a90aad63a2f7b64ce
AdelNoroozi <adel13no@gmail.com>
2023-09-05 09:35:16 +0330

    fix(back): modification in mobile prop table

--- models/mobile_prop.py
+++ models/mobile_prop.py
@@ -1,6 +1,10 @@
 import sqlalchemy
 
+from db import metadata
+
 mobile_prop = sqlalchemy.Table(
+    "mobile_props",
+    metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("prop", sqlalchemy.String(30), nullable=False),
     sqlalchemy.Column("is_selectable", sqlalchemy.Boolean, server_default=False),
