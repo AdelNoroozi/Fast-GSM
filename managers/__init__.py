40bb6ce9e2cd753dca0ef88ada7418de0d199199
AdelNoroozi <adel13no@gmail.com>
2023-09-05 11:39:26 +0330

    feat(back): user manager and register function added

---
+++ managers/__init__.py
@@ -0,0 +1,3 @@
+from managers.auth import AuthManager
+from managers.auth import CustomHTTPBearer
+from managers.user import UserManager
---
+++ managers/user.py
@@ -0,0 +1,21 @@
+from fastapi import HTTPException
+from passlib import context
+
+from db import database
+from managers import AuthManager
+from models import user
+from asyncpg import UniqueViolationError
+pwd_context = context.CryptContext(schemes=["bcrypt"], deprecated=True)
+
+
+class UserManager:
+    @staticmethod
+    async def register(user_data):
+        user_data["password"] = pwd_context.hash(user_data["password"])
+        try:
+            query = user.insert().values(**user_data)
+            id_ = await database.execute(query)
+        except UniqueViolationError:
+            raise HTTPException(401, "duplicate email")
+        user_instance = await database.fetch_one(user.select().where(user.c.id == id_))
+        return AuthManager.encode_token(user_instance)
