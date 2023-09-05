9adf4f6ba6316514601c6498ac4ce8825d8c3b09
AdelNoroozi <adel13no@gmail.com>
2023-09-05 14:31:53 +0330

    feat(back): status code added to APIs

--- resources/auth.py
+++ resources/auth.py
@@ -5,13 +5,13 @@ from schemas.request import RegisterModel, LoginModel
 router = APIRouter(tags=["Auth"])
 
 
-@router.post("/register/")
+@router.post("/register/", status_code=201)
 async def register(user_data: RegisterModel):
     token = await UserManager.register(user_data.model_dump())
     return {"token": token}
 
 
-@router.post("/login/")
+@router.post("/login/", status_code=200)
 async def login(user_data: LoginModel):
     token = await UserManager.login(user_data.model_dump())
     return {"token": token}
