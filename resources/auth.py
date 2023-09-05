3f85258812df0d0bf044b8d2e1f051336d428eed
AdelNoroozi <adel13no@gmail.com>
2023-09-05 14:13:16 +0330

    feat(back): login API added

--- resources/auth.py
+++ resources/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from managers import UserManager
-from schemas.request import RegisterModel
+from schemas.request import RegisterModel, LoginModel
 
 router = APIRouter(tags=["Auth"])
 
@@ -8,4 +8,10 @@ router = APIRouter(tags=["Auth"])
 @router.post("/register/")
 async def register(user_data: RegisterModel):
     token = await UserManager.register(user_data.model_dump())
+    return {"token": token}
+
+
+@router.post("/login/")
+async def login(user_data: LoginModel):
+    token = await UserManager.login(user_data.model_dump())
     return {"token": token}
