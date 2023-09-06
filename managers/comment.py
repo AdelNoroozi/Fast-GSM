19a057ec09a044d95249b0cad93591b7466692d5
Adel <adel13no@gmail.com>
2023-09-06 12:12:28 +0430

    fix(back): comment manager debugged

--- managers/comment.py
+++ managers/comment.py
@@ -6,12 +6,12 @@ class CommentManager:
     @staticmethod
     async def create_comment(comment_data, requesting_user):
         comment_data["user_id"] = requesting_user["id"]
-        query = comment.insert.values(**comment_data)
+        query = comment.insert().values(**comment_data)
         id_ = await database.execute(query)
         response_query = (
             comment.join(user, comment.c.user_id == user.c.id)
             .join(mobile, comment.c.mobile_id == mobile.c.id)
             .select()
-            .where(mobile.c.id == id_)
+            .where(comment.c.id == id_)
         )
         return await database.fetch_one(response_query)
