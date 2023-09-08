21ede8f01bddfbb7dd5356cd2de01486cc0682df
Adel <adel13no@gmail.com>
2023-09-08 10:49:15 +0430

    feat(back): proper comment response for non existence mobiles added

--- managers/comment.py
+++ managers/comment.py
@@ -1,3 +1,6 @@
+from asyncpg import ForeignKeyViolationError
+from fastapi import HTTPException
+
 from db import database
 from models import comment, user, mobile
 
@@ -6,12 +9,15 @@ class CommentManager:
     @staticmethod
     async def create_comment(comment_data, requesting_user):
         comment_data["user_id"] = requesting_user["id"]
-        query = comment.insert().values(**comment_data)
-        id_ = await database.execute(query)
+        try:
+            query = comment.insert().values(**comment_data)
+            id_ = await database.execute(query)
+        except ForeignKeyViolationError:
+            raise HTTPException(404, "mobile not found")
         response_query = (
             comment.join(user, comment.c.user_id == user.c.id)
-            .join(mobile, comment.c.mobile_id == mobile.c.id)
-            .select()
-            .where(comment.c.id == id_)
+                .join(mobile, comment.c.mobile_id == mobile.c.id)
+                .select()
+                .where(comment.c.id == id_)
         )
         return await database.fetch_one(response_query)
