68626d65e05a6da95829d0bae60ce86b0c676370
AdelNoroozi <adel13no@gmail.com>
2023-09-10 13:27:26 +0330

    feat(back): remove like logic added

--- managers/like.py
+++ managers/like.py
@@ -10,7 +10,12 @@ class LikeManager:
     async def like_mobile(like_data, requesting_user):
         like_data["user_id"] = requesting_user["id"]
         try:
-            query = like.insert().values(**like_data)
-            await database.execute(query)
+            db_like = await database.fetch_one(like.select().where(like.c.user_id == like_data["user_id"],
+                                                                   like.c.mobile_id == like_data["mobile_data"]))
+            if db_like:
+                query = like.delete().where(like.c.id == db_like["id"])
+            else:
+                query = like.insert().values(**like_data)
         except ForeignKeyViolationError:
             raise HTTPException(404, "mobile not found")
+        await database.execute(query)
