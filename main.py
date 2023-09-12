1fbedabcd96f924a50db0976f4f82510154bad01
AdelNoroozi <adel13no@gmail.com>
2023-09-12 09:03:10 +0330

    feat(back): CORS added

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from db import database
 from resources import api_router
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
 
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.on_event("startup")
