1e5f5a2e7fa76182e48f08e65ea2369ff388cc42
Adel <adel13no@gmail.com>
2023-09-16 21:55:50 +0430

    feat(back): dependencies added to create prop API

--- resources/mobile_prop.py
+++ resources/mobile_prop.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from managers import MobilePropManager
+from managers import MobilePropManager, is_admin, oauth2_scheme
 from schemas.request import CreatePropModel
 
 router = APIRouter(tags=["Mobile Prop"])
@@ -11,6 +11,6 @@ async def list_props():
     return await MobilePropManager.get_props()
 
 
-@router.post("/props/", status_code=201)
+@router.post("/props/", status_code=201, dependencies=[Depends(oauth2_scheme), Depends(is_admin)])
 async def list_props(prop_data: CreatePropModel):
     return await MobilePropManager.create_props(prop_data.model_dump())
