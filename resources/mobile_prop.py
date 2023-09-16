f063502868a454e782bcb01998b1c770b49d10b5
Adel <adel13no@gmail.com>
2023-09-16 21:53:39 +0430

    feat(back): create prop API added

--- resources/mobile_prop.py
+++ resources/mobile_prop.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from managers import MobilePropManager
-from schemas.response import MobilePropModel
+from schemas.request import CreatePropModel
 
 router = APIRouter(tags=["Mobile Prop"])
 
@@ -9,3 +9,8 @@ router = APIRouter(tags=["Mobile Prop"])
 @router.get("/props/", status_code=200)
 async def list_props():
     return await MobilePropManager.get_props()
+
+
+@router.post("/props/", status_code=201)
+async def list_props(prop_data: CreatePropModel):
+    return await MobilePropManager.create_props(prop_data.model_dump())
