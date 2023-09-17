c641b2baa52c75c4b1731108e69db599266ef255
AdelNoroozi <adel13no@gmail.com>
2023-09-17 09:20:45 +0330

    feat(back): modification on create mobile request model schema for adding photos

--- schemas/request/mobile.py
+++ schemas/request/mobile.py
@@ -8,10 +8,16 @@ class InputPropModel(BaseModel):
     value: str
 
 
+class MobilePhotoModel(BaseModel):
+    photo_url: str
+    alt_text: str
+
+
 class CreateMobileModel(BaseModel):
     name: str
     release_date: date
     brand_id: int
     price: float
+    photos: list[MobilePhotoModel]
     props: list[int]
     input_props: list[InputPropModel]
